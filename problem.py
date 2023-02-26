1ebf67c9249875098a5c251c6a81bdede856f36e
Ihssane Ghalas <ihssaneghalas123@gmail.com>
2023-02-26 12:38:11 +0100

    Chnaging the score

--- problem.py
+++ problem.py
@@ -14,7 +14,6 @@ problem_title = "Scam prediction on Reddit"
 # --------------------------------------------------
 
 _target_column_name = 'label'
-
 _prediction_label_names = [0, 1]
 
 Predictions = rw.prediction_types.make_multiclass(
@@ -23,21 +22,12 @@ Predictions = rw.prediction_types.make_multiclass(
 
 workflow = rw.workflows.Estimator()
 
-score_type_1 = rw.score_types.ROCAUC(name='auc')
-score_type_2 = rw.score_types.Accuracy(name='acc')
-score_type_3 = rw.score_types.F1Above(name='f1_above')
-
 score_types = [
-    # The official score combines the three scores with weights 1/4, 1/4, 2/4
-    rw.score_types.Combined(
-        name='combined', score_types=[score_type_1, score_type_2, score_type_3],
-        weights=[1./4, 1./4, 2./4], precision=3),
+    rw.score_types.F1Above(name='f1_above')
 ]
 
-
-
 def get_cv(X, y):
-    cv = ShuffleSplit(n_splits=10, test_size=0.25, random_state=57)
+    cv = ShuffleSplit(n_splits=2, test_size=0.30, random_state=42)
     return cv.split(X, y)
 
 def _get_data(path, f_name):
@@ -47,14 +37,10 @@ def _get_data(path, f_name):
 
     return X, y
 
-
 def get_train_data(path="."):
     f_name = "train.csv"
     return _get_data(path, f_name)
 
-
 def get_test_data(path="."):
     f_name = "test.csv"
     return _get_data(path, f_name)
-
-
